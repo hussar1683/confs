4a662b256a534392ce2f5fe298b926df141889c6
hussar1683 <380012730@qq.com>
2022-01-18 11:26:59 +0800

    update for ss

--- ubuntu_server/ss_one_stop.sh
+++ ubuntu_server/ss_one_stop.sh
@@ -6,7 +6,8 @@ set -x
 
 cd /opt
 
-apt-get install python-pip
+apt-get update -y
+apt-get install -y vim git python-pip python-setuptools
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 
 sudo ssserver -p port -k password -m aes-256-cfb --user nobody -d start
